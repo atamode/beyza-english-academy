54937043813e50a77d8ba88963298719cf82cd37
atamode <atacetinkayali@gmail.com>
2026-08-07 20:04:40 +0300

    feat(poma-shift): add production analytics config

---
+++ games/poma-shift/analytics-config.js
@@ -0,0 +1,8 @@
+(() => {
+  const existing = window.PomaShiftAnalyticsConfig || {};
+  window.PomaShiftAnalyticsConfig = {
+    measurementId: 'G-LVDEFW23S9',
+    testing: false,
+    ...existing,
+  };
+})();
